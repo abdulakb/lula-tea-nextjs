32d794181d397f8da7aa28d91bc63e7511c30f93
Abdul Akb <abdulb@microsoft.com>
2025-12-08 07:56:23 +0300

    Fix duplicate Image import

--- lula-tea-nextjs/app/story/page.tsx
+++ lula-tea-nextjs/app/story/page.tsx
@@ -5,7 +5,6 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import StoryCarousel from "../components/StoryCarousel";
 import Image from "next/image";
-import Image from "next/image";
 
 export default function StoryPage() {
   const { language, t } = useLanguage();
